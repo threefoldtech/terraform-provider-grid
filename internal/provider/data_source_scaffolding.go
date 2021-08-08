bcb85bfbb52556407c2636cd6bf7f2bca882ca4e
Ashraf Fouda <ashraf.m.fouda@gmail.com>
2021-08-08 17:00:37 +0200

    Adding base provider
    Signed-off-by: Ashraf Fouda <ashraf.m.fouda@gmail.com>

--- internal/provider/data_source_scaffolding.go
+++ internal/provider/data_source_scaffolding.go
@@ -7,25 +7,44 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-func dataSourceScaffolding() *schema.Resource {
+func dataSourceDisk() *schema.Resource {
 	return &schema.Resource{
 		// This description is used by the documentation generator and the language server.
 		Description: "Sample data source in the Terraform provider scaffolding.",
 
-		ReadContext: dataSourceScaffoldingRead,
+		ReadContext: dataSourceDiskRead,
 
 		Schema: map[string]*schema.Schema{
-			"sample_attribute": {
-				// This description is used by the documentation generator and the language server.
-				Description: "Sample attribute.",
+			"id": {
+				Description: "Disk ID",
 				Type:        schema.TypeString,
-				Required:    true,
+				Computed:    true,
+			},
+			"name": {
+				Description: "Disk Name",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"version": {
+				Description: "Version",
+				Type:        schema.TypeInt,
+				Computed:    true,
+			},
+			"description": {
+				Description: "Description field",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"size": {
+				Description: "Disk size in Gigabytes",
+				Type:        schema.TypeInt,
+				Computed:    true,
 			},
 		},
 	}
 }
 
-func dataSourceScaffoldingRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+func dataSourceDiskRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	// use the meta value to retrieve your client from the provider configure method
 	// client := meta.(*apiClient)
 
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -2,9 +2,11 @@ package provider
 
 import (
 	"context"
+	"encoding/hex"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/threefoldtech/zos/pkg/rmb"
 )
 
 func init() {
@@ -26,32 +28,78 @@ func init() {
 func New(version string) func() *schema.Provider {
 	return func() *schema.Provider {
 		p := &schema.Provider{
+			Schema: map[string]*schema.Schema{
+				"twin_id": &schema.Schema{
+					Type:        schema.TypeInt,
+					Required:    true,
+					DefaultFunc: schema.EnvDefaultFunc("TWIN_ID", 0),
+				},
+				"seed": &schema.Schema{
+					Type:        schema.TypeString,
+					Required:    true,
+					Sensitive:   true,
+					DefaultFunc: schema.EnvDefaultFunc("SEED", nil),
+				},
+				"substrate_url": &schema.Schema{
+					Type:        schema.TypeString,
+					Required:    true,
+					DefaultFunc: schema.EnvDefaultFunc("SUBSTRATE_URL", "wss://explorer.devnet.grid.tf/ws"),
+				},
+				"rmb_url": &schema.Schema{
+					Type:        schema.TypeString,
+					Required:    true,
+					DefaultFunc: schema.EnvDefaultFunc("RMB_URL", "tcp://127.0.0.1:6379"),
+				},
+			},
 			DataSourcesMap: map[string]*schema.Resource{
-				"scaffolding_data_source": dataSourceScaffolding(),
+				"scaffolding_data_source": dataSourceDisk(),
 			},
 			ResourcesMap: map[string]*schema.Resource{
-				"scaffolding_resource": resourceScaffolding(),
+				"grid_disk": resourceDisk(),
 			},
 		}
 
-		p.ConfigureContextFunc = configure(version, p)
+		p.ConfigureContextFunc = providerConfigure
 
 		return p
 	}
 }
 
 type apiClient struct {
-	// Add whatever fields, client or connection info, etc. here
-	// you would need to setup to communicate with the upstream
-	// API.
+	twin_id       uint32
+	seed          []byte
+	substrate_url string
+	rmb_url       string
+	client        rmb.Client
 }
 
-func configure(version string, p *schema.Provider) func(context.Context, *schema.ResourceData) (interface{}, diag.Diagnostics) {
-	return func(context.Context, *schema.ResourceData) (interface{}, diag.Diagnostics) {
-		// Setup a User-Agent for your API client (replace the provider name for yours):
-		// userAgent := p.UserAgent("terraform-provider-scaffolding", version)
-		// TODO: myClient.UserAgent = userAgent
+// func configure(version string, p *schema.Provider) func(context.Context, *schema.ResourceData) (interface{}, diag.Diagnostics) {
+// 	return func(context.Context, *schema.ResourceData) (interface{}, diag.Diagnostics) {
+// 		// Setup a User-Agent for your API client (replace the provider name for yours):
+// 		// userAgent := p.UserAgent("terraform-provider-scaffolding", version)
+// 		myClient = apiClient()
+// 		p.user
 
-		return &apiClient{}, nil
+// 		return &apiClient{}, nil
+// 	}
+// }
+
+func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+	seed, err := hex.DecodeString(d.Get("seed").(string))
+	if err != nil {
+		panic(err)
 	}
+	apiClient := apiClient{}
+	apiClient.twin_id = uint32(d.Get("twin_id").(int))
+	apiClient.substrate_url = d.Get("substrate_url").(string)
+	apiClient.rmb_url = d.Get("rmb_url").(string)
+	apiClient.seed = seed
+	cl, err := rmb.NewClient(apiClient.rmb_url)
+
+	if err != nil {
+		panic(err)
+	}
+	apiClient.client = cl
+
+	return &apiClient, nil
 }
