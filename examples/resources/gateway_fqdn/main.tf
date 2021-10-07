e1707edaab4ef3fa95df2edbe6cf87e9ad21fe9a
OmarElawady <omarelawady1998@gmail.com>
2021-10-07 16:33:43 +0200

    Devnet upgrade (#66)
    * devnet upgrade

--- examples/resources/gateway_fqdn/main.tf
+++ examples/resources/gateway_fqdn/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     grid = {
       source = "threefoldtech/grid"
-      version = "0.1.2"
+      version = "0.1.8"
     }
   }
 }
@@ -12,7 +12,7 @@ provider "grid" {
 
 
 resource "grid_fqdn_proxy" "p1" {
-  node = 2
+  node = 40
   name = "workloadname"
   fqdn = "remote.omar.grid.tf"
   backends = [format("https://137.184.106.152:443")]
