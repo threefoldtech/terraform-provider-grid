e149ff8b3c62533786f7c557b2be5e8c82eb9daf
OmarElawady <omarelawady1998@gmail.com>
2021-11-14 15:33:39 +0200

    remove wg config from outputs & fix typos

--- examples/resources/multinode/main.tf
+++ examples/resources/multinode/main.tf
@@ -54,16 +54,12 @@ resource "grid_deployment" "d2" {
   }
 }
 
-output "wg_config" {
-    value = grid_network.net1.access_wg_config
-}
-
-output "noed1_container1_ip" {
+output "node1_container1_ip" {
     value = grid_deployment.d1.vms[0].ip
 }
 
 
-output "noed2_container1_ip" {
+output "node2_container1_ip" {
     value = grid_deployment.d2.vms[0].ip
 }
 
