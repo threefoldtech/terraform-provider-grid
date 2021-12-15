7f794c2cc8d129158b0c3491888771aa3ff40aa2
Ashraf Fouda <ashraf.m.fouda@gmail.com>
2021-12-15 17:16:46 +0200

    Adds deploy peertube with yggdrasil ip
    Signed-off-by: Ashraf Fouda <ashraf.m.fouda@gmail.com>

--- examples/resources/peertube/main.tf
+++ examples/resources/peertube/main.tf
@@ -15,7 +15,7 @@
   # - the fqdn can be computed from grid_gateway_domain for the vm
   # - the backend can reference the vm ip directly 
   data "grid_gateway_domain" "domain" {
-    node = 7 
+    node = 8
     name = "ashrafpeertube"
   }
   resource "grid_network" "net1" {
@@ -31,30 +31,31 @@
     ip_range = lookup(grid_network.net1.nodes_ip_range, 7, "")
     vms {
       name = "vm1"
-      flist = "https://hub.grid.tf/omarabdul3ziz.3bot/threefoldtech-peertube-v3.0.flist"
+      flist = "https://hub.grid.tf/omarabdul3ziz.3bot/threefoldtech-peertube-v3.0.2.flist"
       cpu = 2 
-      publicip = true
+      # publicip = true
       entrypoint = "/usr/local/bin/entrypoint.sh"
       memory = 4096
       env_vars = {
         SSH_KEY = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDTwULSsUubOq3VPWL6cdrDvexDmjfznGydFPyaNcn7gAL9lRxwFbCDPMj7MbhNSpxxHV2+/iJPQOTVJu4oc1N7bPP3gBCnF51rPrhTpGCt5pBbTzeyNweanhedkKDsCO2mIEh/92Od5Hg512dX4j7Zw6ipRWYSaepapfyoRnNSriW/s3DH/uewezVtL5EuypMdfNngV/u2KZYWoeiwhrY/yEUykQVUwDysW/xUJNP5o+KSTAvNSJatr3FbuCFuCjBSvageOLHePTeUwu6qjqe+Xs4piF1ByO/6cOJ8bt5Vcx0bAtI8/MPApplUU/JWevsPNApvnA/ntffI+u8DCwgP ashraf@thinkpad"
-        PEERTUBE_DB_SUFFIX="_prod"
-        PEERTUBE_DB_USERNAME="peertube"
-        PEERTUBE_DB_PASSWORD="peertube"
-        PEERTUBE_ADMIN_EMAIL="support@threefold.com"
+        PEERTUBE_DB_SUFFIX = "_prod"
+        PEERTUBE_DB_USERNAME = "peertube"
+        PEERTUBE_DB_PASSWORD = "peertube"
+        PEERTUBE_ADMIN_EMAIL = "support@threefold.com"
         PEERTUBE_WEBSERVER_HOSTNAME = data.grid_gateway_domain.domain.fqdn
-        PEERTUBE_WEBSERVER_PORT=443
-        PEERTUBE_SMTP_HOSTNAME="https://app.sendgrid.com"
-        PEERTUBE_SMTP_USERNAME="sendgridusername"
-        PEERTUBE_SMTP_PASSWORD="sendgridpassword"
+        PEERTUBE_WEBSERVER_PORT = 443
+        PEERTUBE_SMTP_HOSTNAME = "https://app.sendgrid.com"
+        PEERTUBE_SMTP_USERNAME = "sendgridusername"
+        PEERTUBE_SMTP_PASSWORD = "sendgridpassword"
+        PEERTUBE_BIND_ADDRESS = "::",
       }
       planetary = true
     }
   }
   resource "grid_name_proxy" "p1" {
-    node = 7
+    node = 8
     name = "ashrafpeertube"
-    backends = [format("http://%s:9000", split("/", grid_deployment.d1.vms[0].computedip)[0])]
+    backends = [format("http://[%s]:9000", grid_deployment.d1.vms[0].ygg_ip)]
     tls_passthrough = false
   }
   output "fqdn" {
@@ -63,9 +64,9 @@
   output "node1_zmachine1_ip" {
       value = grid_deployment.d1.vms[0].ip
   }
-  output "public_ip" {
-      value = split("/",grid_deployment.d1.vms[0].computedip)[0]
-  }
+  # output "public_ip" {
+  #     value = split("/",grid_deployment.d1.vms[0].computedip)[0]
+  # }
 
   output "ygg_ip" {
       value = grid_deployment.d1.vms[0].ygg_ip
