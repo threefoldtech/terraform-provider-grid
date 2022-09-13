1346ec2f003332a9290a4b522d9be31fac4d9b3b
IslamWalid <islamwalid83@gmail.com>
2022-09-13 15:46:45 +0200

    Test kubernetes deployment using k8s module

--- tests/kubernetes_with_k8s_module/kubernetes_test.go
+++ tests/kubernetes_with_k8s_module/kubernetes_test.go
@@ -1,26 +1,126 @@
 package test
 
 import (
+	"fmt"
+	"os"
+	"strings"
 	"testing"
+	"time"
 
-    "github.com/stretchr/testify/assert"
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+	"github.com/threefoldtech/terraform-provider-grid/tests"
 )
 
 func TestKubernetesDeployment(t *testing.T) {
-    terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-        TerraformDir: "./",
-        Parallelism: 1,
-    })
-    defer terraform.Destroy(t, terraformOptions)
+	tests.SshKeys()
+	sshKey := os.Getenv("PUBLICKEY")
 
-    terraform.InitAndApply(t, terraformOptions)
-    out := terraform.Output(t, terraformOptions, "master_yggip")
-    assert.NotEmpty(t, out)
+    file, _ := os.Create("verbose.txt")
+    defer file.Close()
 
-    out = terraform.Output(t, terraformOptions, "w0_yggip")
-    assert.NotEmpty(t, out)
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "./",
+		Parallelism:  1,
+		Vars: map[string]interface{}{
+			"ssh":           sshKey,
+			"network_nodes": []int{45, 49},
+			"workers": []map[string]interface{}{
+				{
+					"name":        "w0",
+					"node":        49,
+					"cpu":         1,
+					"memory":      1024,
+					"disk_name":   "w0disk",
+					"mount_point": "/mydisk",
+					"publicip":    false,
+					"planetary":   true,
+				},
+			},
+			"disks": []map[string]interface{}{
+				{
+					"name":        "mrdisk",
+					"node":        45,
+					"size":        5,
+					"description": "",
+				},
+				{
+					"name":        "w0disk",
+					"node":        49,
+					"size":        2,
+					"description": "",
+				},
+			},
+		},
+	})
 
-    out = terraform.Output(t, terraformOptions, "w1_yggip")
-    assert.NotEmpty(t, out)
+	terraform.InitAndApply(t, terraformOptions)
+    assertDeploymentStatus(t, terraformOptions, file)
+
+	terraformOptions.Vars["workers"] = []map[string]interface{} {
+        {
+            "name":        "w0",
+            "node":        49,
+            "cpu":         1,
+            "memory":      1024,
+            "disk_name":   "w0disk",
+            "mount_point": "/mydisk",
+            "publicip":    false,
+            "planetary":   true,
+        },
+        {
+            "name":        "w1",
+            "node":        49,
+            "cpu":         1,
+            "memory":      1024,
+            "disk_name":   "w1disk",
+            "mount_point": "/mydisk",
+            "publicip":    false,
+            "planetary":   true,
+        },
+    }
+	terraformOptions.Vars["disks"] = []map[string]interface{} {
+        {
+            "name":        "mrdisk",
+            "node":        45,
+            "size":        5,
+            "description": "",
+        },
+        {
+            "name":        "w0disk",
+            "node":        49,
+            "size":        2,
+            "description": "",
+        },
+        {
+            "name":        "w1disk",
+            "node":        49,
+            "size":        2,
+            "description": "",
+        },
+    }
+
+	terraform.Apply(t, terraformOptions)
+    assertDeploymentStatus(t, terraformOptions, file)
+	terraform.Destroy(t, terraformOptions)
+}
+
+func assertDeploymentStatus(t *testing.T, terraformOptions *terraform.Options, file *os.File) {
+    t.Helper()
+
+	masterYggIP := terraform.Output(t, terraformOptions, "master_yggip")
+	assert.NotEmpty(t, masterYggIP)
+    fmt.Fprintln(file, masterYggIP)
+
+	res, err := tests.RemoteRun("root", masterYggIP, "kubectl get node")
+	assert.Empty(t, err)
+
+    time.Sleep(3 * time.Second)
+	nodes := strings.Split(res, "\n")[1:]
+    fmt.Fprintf(file, "All nodes: %#v\n\n", nodes)
+
+	for _, node := range nodes {
+        fmt.Fprintln(file, node)
+		assert.Contains(t, node, "Ready")
+	}
 }
--- tests/kubernetes_with_k8s_module/main.tf
+++ tests/kubernetes_with_k8s_module/main.tf
@@ -1,19 +1,48 @@
 module "kubernetes" {
     source = "github.com/IslamWalid/terraform-provider-grid/modules/k8s-module"
-    ssh = local.ssh
     token = local.token
     network = local.network
     master = local.master
-    workers = local.workers
-    disks = local.disks
+    ssh = var.ssh
+    workers = var.workers
+    disks = var.disks
+}
+
+variable "ssh" {
+    type = string
+}
+
+variable "network_nodes" {
+    type = list(number)
+}
+
+variable "workers" {
+    type = list(object({
+        name = string
+        node = number
+        cpu = number
+        memory = number
+        disk_name = string
+        mount_point = string
+        publicip = bool
+        planetary = bool
+    }))
+}
+
+variable "disks" {
+    type = list(object({
+        node = number
+        name = string
+        size = number
+        description = string
+    }))
 }
 
 locals {
-    ssh = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC+/mcyN8lmXYY0/8+irXsYpL6uSQHAG/Tulg4O610A3RnUOKt3F42SuTtGDu1uvQX/vdnb+MgXnwLy+zsOe3YISUgvXWJQJOgMvphkisHyfCFeYDE8NyGRpCmlsuKr0jsj3fmyuCAV5TXJWRCKEOxU7wdPUeGC3+VhOFTI7JOHLdT06IX1wznekj+bKUZKbQHV5d4MTHo9dmoQirQU4AyrIMC0K2jHUCMJByLs81evYaplfZmLNbtDW/3KbKa+lh2NovCAbtvu1mC+GgELnOSm7RQ7AEta+a5BEnCEg9sYjZ2PlVt3pihogWtnzkEkd7/cmTk3exrDX86emZSga+NWaI+/mQODpdDsWStetwVIo1WpVdmJLmviPGcwXXx5unDYqFqkJ9F+OnbedCFh/U/9+tSg1/2BsKo81N9zNpoprQCPCKtHgLDbEnHaL7D1Xx2b9/8GD84ADaRr55f34L9mLHvaBRRvZ8L4Jl845KuJ9GCEkmirBHCtdSoIZrWqAbE= islam@islam"
     token = "838a6db4"
 
     network = {
-        nodes = [45, 33, 30]
+        nodes = var.network_nodes
         ip_range = "10.1.0.0/16"
         name = "test_network"
         description = "new network for testing"
@@ -30,60 +59,8 @@ locals {
         publicip = false
         planetary = true
     }
-
-    workers = [
-        {
-            name = "w0"
-            node = 33
-            cpu = 1
-            memory = 1024
-            disk_name = "w0disk"
-            mount_point = "/mydisk"
-            publicip = false
-            planetary = true
-        },
-        {
-            name = "w1"
-            node = 30
-            cpu = 1
-            memory = 2048
-            disk_name = "w1disk"
-            mount_point = "/mydisk"
-            publicip = false
-            planetary = true
-        }
-    ]
-
-    disks = [
-        {
-            name = "mrdisk"
-            node = 45
-            size = 5
-            description = ""
-        },
-        {
-            name = "w0disk"
-            node = 33
-            size = 2
-            description = ""
-        },
-        {
-            name = "w1disk"
-            node = 30
-            size = 3
-            description = ""
-        }
-    ]
 }
 
 output "master_yggip" {
     value = module.kubernetes.master.ygg_ip
-}
-
-output "w0_yggip" {
-    value = module.kubernetes.workers["w0"].ygg_ip
-}
-
-output "w1_yggip" {
-    value = module.kubernetes.workers["w1"].ygg_ip
 }
