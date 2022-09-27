bf4503098e7501e914e79af4b03cd1e7ad2fab4a
IslamWalid <islamwalid83@gmail.com>
2022-09-27 15:34:58 +0200

    Add k8s module test

---
+++ tests/kubernetes_with_k8s_module/kubernetes_test.go
@@ -0,0 +1,120 @@
+package test
+
+import (
+	"os"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+	"github.com/threefoldtech/terraform-provider-grid/tests"
+)
+
+func TestKubernetesDeployment(t *testing.T) {
+	tests.SshKeys()
+	sshKey := os.Getenv("PUBLICKEY")
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "./",
+		Parallelism:  1,
+		Vars: map[string]interface{}{
+			"ssh":           sshKey,
+			"network_nodes": []int{45, 49},
+			"workers": []map[string]interface{}{
+				{
+					"name":        "w0",
+					"node":        49,
+					"cpu":         1,
+					"memory":      1024,
+					"disk_name":   "w0disk",
+					"mount_point": "/mydisk",
+					"publicip":    false,
+					"planetary":   true,
+				},
+			},
+			"disks": []map[string]interface{}{
+				{
+					"name":        "mrdisk",
+					"node":        45,
+					"size":        5,
+					"description": "",
+				},
+				{
+					"name":        "w0disk",
+					"node":        49,
+					"size":        2,
+					"description": "",
+				},
+			},
+		},
+	})
+
+	terraform.InitAndApply(t, terraformOptions)
+    assertDeploymentStatus(t, terraformOptions)
+
+	terraformOptions.Vars["workers"] = []map[string]interface{} {
+        {
+            "name":        "w0",
+            "node":        49,
+            "cpu":         1,
+            "memory":      1024,
+            "disk_name":   "w0disk",
+            "mount_point": "/mydisk",
+            "publicip":    false,
+            "planetary":   true,
+        },
+        {
+            "name":        "w1",
+            "node":        49,
+            "cpu":         1,
+            "memory":      1024,
+            "disk_name":   "w1disk",
+            "mount_point": "/mydisk",
+            "publicip":    false,
+            "planetary":   true,
+        },
+    }
+	terraformOptions.Vars["disks"] = []map[string]interface{} {
+        {
+            "name":        "mrdisk",
+            "node":        45,
+            "size":        5,
+            "description": "",
+        },
+        {
+            "name":        "w0disk",
+            "node":        49,
+            "size":        2,
+            "description": "",
+        },
+        {
+            "name":        "w1disk",
+            "node":        49,
+            "size":        2,
+            "description": "",
+        },
+    }
+
+	terraform.Apply(t, terraformOptions)
+    assertDeploymentStatus(t, terraformOptions)
+	terraform.Destroy(t, terraformOptions)
+}
+
+func assertDeploymentStatus(t *testing.T, terraformOptions *terraform.Options) {
+    t.Helper()
+
+	masterYggIP := terraform.Output(t, terraformOptions, "master_yggip")
+	assert.NotEmpty(t, masterYggIP)
+
+    time.Sleep(5 * time.Second)
+	res, err := tests.RemoteRun("root", masterYggIP, "kubectl get node")
+    res = strings.Trim(res, "\n")
+	assert.Empty(t, err)
+
+	nodes := strings.Split(res, "\n")[1:]
+
+	for _, node := range nodes {
+		assert.Contains(t, node, "Ready")
+	}
+}
