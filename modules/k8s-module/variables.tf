7e11e5e3cf466431c6a1c2ad5e22b62faf80fa87
IslamWalid <islamwalid83@gmail.com>
2022-09-27 14:33:46 +0200

    Add k8s module

---
+++ modules/k8s-module/variables.tf
@@ -0,0 +1,64 @@
+variable "ssh" {
+    type = string
+}
+
+variable "token" {
+    type = string
+}
+
+variable "network" {
+    type = object({
+        nodes = list(number)
+        name = string
+        ip_range = string
+        description = string
+        add_wg_access = bool
+    })
+}
+
+variable "master" {
+    type = object({
+        name = string
+        node = number
+        cpu = number
+        memory = number
+        disk_name = string
+        mount_point = string
+        publicip = bool
+        planetary = bool
+    })
+
+    validation {
+        condition = var.master.memory >= 1024
+        error_message = "Memory must be at least 1GB"
+    }
+}
+
+variable "workers" {
+    type = list(object({
+        name = string
+        node = number
+        cpu = number
+        memory = number
+        disk_name = string
+        mount_point = string
+        publicip = bool
+        planetary = bool
+    }))
+
+    validation {
+        condition = ([
+            for w in var.workers: true if w.memory >= 1024
+        ]) != length(var.workers)
+        error_message = "Memory must be at least 1GB"
+    }
+}
+
+variable "disks" {
+    type = list(object({
+        node = number
+        name = string
+        size = number
+        description = string
+    }))
+}
