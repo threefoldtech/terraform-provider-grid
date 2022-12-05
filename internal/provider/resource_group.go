70344415ce3a9befc33b616e4510afa16ab812e5
Ashraf Fouda <ashraf.m.fouda@gmail.com>
2022-12-05 12:55:42 +0200

    Adds resource group impl
    Signed-off-by: Ashraf Fouda <ashraf.m.fouda@gmail.com>

--- internal/provider/resource_group.go
+++ internal/provider/resource_group.go
@@ -2,9 +2,12 @@ package provider
 
 import (
 	"context"
+	"strconv"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/pkg/errors"
+	"github.com/threefoldtech/substrate-client"
 )
 
 func resourceGroup() *schema.Resource {
@@ -23,17 +26,52 @@ func resourceGroup() *schema.Resource {
 }
 
 func resourceGroupRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cl := meta.(apiClient)
+	var diags diag.Diagnostics
+	group_id, err := strconv.ParseUint(d.Id(), 10, 32)
+	if err != nil {
+		diags = append(diags, diag.FromErr(err)...)
+	}
+	_, err = cl.substrateConn.GetGroup(group_id)
+	if err != nil && errors.Is(err, substrate.ErrNotFound) {
+		d.SetId("")
+	} else if err != nil {
+		diags = append(diags, diag.FromErr(err)...)
+		return diags
+	}
 	return nil
 }
 
 func resourceGroupCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cl := meta.(apiClient)
+	var diags diag.Diagnostics
+	group_id, err := cl.substrateConn.CreateGroup(cl.identity)
+	if err != nil {
+		diags = append(diags, diag.FromErr(err)...)
+	}
+
+	d.SetId(strconv.Itoa(int(group_id)))
 	return nil
 }
 
 func resourceGroupUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	diags = append(diags, diag.FromErr(errors.New("Updating groups in not allowed"))...)
 	return nil
 }
 
 func resourceGroupDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	cl := meta.(apiClient)
+	var diags diag.Diagnostics
+	group_id, err := strconv.ParseUint(d.Id(), 10, 32)
+	if err != nil {
+		diags = append(diags, diag.FromErr(err)...)
+	}
+	err = cl.substrateConn.DeleteGroup(cl.identity, uint32(group_id))
+	if err != nil {
+		diags = append(diags, diag.FromErr(err)...)
+	}
+
+	d.SetId("")
 	return nil
 }
