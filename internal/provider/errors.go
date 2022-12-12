e6f5708c728828cf6a6bf6030ce374a6fca9d75e
Ashraf Fouda <ashraf.m.fouda@gmail.com>
2022-12-12 16:29:09 +0200

    added k8s updates (#406)
    * Adds k8s update
    
    Signed-off-by: Ashraf Fouda <ashraf.m.fouda@gmail.com>
    
    * Adds set error handling
    
    Signed-off-by: Ashraf Fouda <ashraf.m.fouda@gmail.com>
    
    Signed-off-by: Ashraf Fouda <ashraf.m.fouda@gmail.com>

--- internal/provider/errors.go
+++ internal/provider/errors.go
@@ -9,9 +9,18 @@ type errSet struct {
 func (e *errSet) Error() string {
 	errStr := "plugin encountered one or more errors while setting state: ("
 	errStr += strings.Join(e.errs, ", ") + ")"
-	return ""
+	return errStr
 }
 
 func (e *errSet) Push(err error) {
-	e.errs = append(e.errs, err.Error())
+	if err != nil {
+		e.errs = append(e.errs, err.Error())
+	}
+}
+
+func (e *errSet) error() error {
+	if e.errs == nil {
+		return nil
+	}
+	return e
 }
