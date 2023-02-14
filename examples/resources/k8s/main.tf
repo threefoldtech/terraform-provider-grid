ec42027526c841b152c762cc25d4a039acd8c9cc
Ali Chaddad <chaddada@incubaid.com>
2023-02-14 15:03:28 +0200

    fix k8s example

--- examples/resources/k8s/main.tf
+++ examples/resources/k8s/main.tf
@@ -15,7 +15,7 @@ locals {
 resource "grid_network" "net1" {
   solution_type = local.solution_type
   name          = local.name
-  nodes         = grid_capacity_reserver.deployer1.nodes
+  nodes         = [2, 3]
   ip_range      = "10.1.0.0/16"
   description   = "newer network"
   add_wg_access = true
@@ -25,7 +25,6 @@ resource "grid_kubernetes" "k8s1" {
   solution_type  = local.solution_type
   name           = local.name
   network_name   = grid_network.net1.name
-  nodes_ip_range = grid_network.net1.nodes_ip_range
   token          = "12345678910122"
   ssh_key        = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDTwULSsUubOq3VPWL6cdrDvexDmjfznGydFPyaNcn7gAL9lRxwFbCDPMj7MbhNSpxxHV2+/iJPQOTVJu4oc1N7bPP3gBCnF51rPrhTpGCt5pBbTzeyNweanhedkKDsCO2mIEh/92Od5Hg512dX4j7Zw6ipRWYSaepapfyoRnNSriW/s3DH/uewezVtL5EuypMdfNngV/u2KZYWoeiwhrY/yEUykQVUwDysW/xUJNP5o+KSTAvNSJatr3FbuCFuCjBSvageOLHePTeUwu6qjqe+Xs4piF1ByO/6cOJ8bt5Vcx0bAtI8/MPApplUU/JWevsPNApvnA/ntffI+u8DCwgP ashraf@thinkpad"
 
@@ -46,14 +45,14 @@ resource "grid_kubernetes" "k8s1" {
   }
   workers {
     disk_size = 14
-    node      = 4
+    node      = 3
     name      = "w2"
     cpu       = 1
     memory    = 2048
   }
   workers {
     disk_size = 13
-    node      = 4
+    node      = 3
     name      = "w3"
     cpu       = 1
     memory    = 2048
