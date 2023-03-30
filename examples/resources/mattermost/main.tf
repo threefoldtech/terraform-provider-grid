fdd736bf44073b86105a054123dca676db32d986
Ashraf Fouda <ashraf.m.fouda@gmail.com>
2023-03-30 13:25:18 +0200

    updates example to use ssh pubkey from .ssh dir
    Signed-off-by: Ashraf Fouda <ashraf.m.fouda@gmail.com>

--- examples/resources/mattermost/main.tf
+++ examples/resources/mattermost/main.tf
@@ -42,7 +42,7 @@ resource "grid_deployment" "d1" {
     entrypoint = "/sbin/zinit init"
     memory     = 4096
     env_vars = {
-      SSH_KEY      = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDTwULSsUubOq3VPWL6cdrDvexDmjfznGydFPyaNcn7gAL9lRxwFbCDPMj7MbhNSpxxHV2+/iJPQOTVJu4oc1N7bPP3gBCnF51rPrhTpGCt5pBbTzeyNweanhedkKDsCO2mIEh/92Od5Hg512dX4j7Zw6ipRWYSaepapfyoRnNSriW/s3DH/uewezVtL5EuypMdfNngV/u2KZYWoeiwhrY/yEUykQVUwDysW/xUJNP5o+KSTAvNSJatr3FbuCFuCjBSvageOLHePTeUwu6qjqe+Xs4piF1ByO/6cOJ8bt5Vcx0bAtI8/MPApplUU/JWevsPNApvnA/ntffI+u8DCwgP ashraf@thinkpad"
+      SSH_KEY      = file("~/.ssh/id_rsa.pub")
       DB_PASSWORD  = "ashroof"
       SITE_URL     = format("https://%s", data.grid_gateway_domain.domain.fqdn)
       SMTPPASSWORD = "password"
