efba76bcce4deaedc19aa0246ed0941a9b50e50a
rawdaGastan <rody.rawda.rr@gmail.com>
2023-04-18 17:07:30 +0200

    check response

--- integrationtests/gateway_test.go
+++ integrationtests/gateway_test.go
@@ -59,12 +59,14 @@ func TestGateWay(t *testing.T) {
 		response, err := http.Get(fmt.Sprintf("https://%s", fqdn))
 		assert.NoError(t, err)
 
-		body, err := io.ReadAll(response.Body)
-		if body != nil {
-			defer response.Body.Close()
+		if response != nil {
+			body, err := io.ReadAll(response.Body)
+			if body != nil {
+				defer response.Body.Close()
+			}
+			assert.NoError(t, err)
+			assert.Contains(t, string(body), "Directory listing for")
 		}
-		assert.NoError(t, err)
-		assert.Contains(t, string(body), "Directory listing for")
 
 	})
 
